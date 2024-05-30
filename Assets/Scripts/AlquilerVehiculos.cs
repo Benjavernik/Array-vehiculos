7427f8e6495109a4598e62182811fe5eca67594e
Benjavernik <163456747+Benjavernik@users.noreply.github.com>
2024-05-30 15:51:40 -0300

    Algunos ejercicios

---
+++ Assets/Scripts/AlquilerVehiculos.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlquilerVehiculos : MonoBehaviour
+{
+    public GameObject[] vehiculos;
+    public int cantidadvehiculos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetearVehiculos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            DesactivarTodoslosvehiculos();
+            vehiculos[Random.Range(0, vehiculos.Length)].SetActive(true);
+
+                
+        }   
+    }
+
+    void ResetearVehiculos()
+    {
+        DesactivarTodoslosvehiculos();
+
+
+        vehiculos[0].SetActive(true);
+
+    }
+        void DesactivarTodoslosvehiculos()
+        {
+            for (int i = 0; i < vehiculos.Length; i++)
+            {
+                vehiculos[i].SetActive(false);
+          
+        }
+    }
+}
